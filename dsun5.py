c95ca17d176724440259c4505a7a8bd706ac4d03
DS <weisun1225@gmail.com>
2026-08-26 18:08:10 +0000

    Add list assignment

---
+++ dsun5.py
@@ -0,0 +1,30 @@
+#Dawei Sun
+#CTC 389
+# 8-26-26
+
+nums = [22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+
+print("No Loop")
+
+print(nums[0])
+print(nums[1])
+print(nums[2])
+print(nums[3])
+print(nums[4])
+print(nums[5])
+print(nums[6])
+print(nums[7])
+print(nums[8])
+print(nums[9])
+
+print("With Loop")
+
+for i in nums:
+    print(i)
+
+words = ["banana", "peach", "carrot", "tomato", "broccoli"]
+
+for i in words:
+    print(i) 
+
+
