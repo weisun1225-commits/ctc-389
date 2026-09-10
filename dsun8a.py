e0abc4a63fb37c4e1fc77beb7cb542acbc570605
DS <weisun1225@gmail.com>
2026-09-10 00:12:14 +0000

    Add dsun8a

---
+++ dsun8a.py
@@ -0,0 +1,12 @@
+#Dawei Sun
+
+name = input("Enter your name: ")
+
+number = int(input("Enter a number: "))
+
+add = int(input("How much to add: "))
+
+total = number + add
+
+print(name, "you entered", number, "and added", add, "to get a total of", total)
+
